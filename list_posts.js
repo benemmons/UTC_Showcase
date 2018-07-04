a572e24824656a3983dff08cd4199fdde737eb15
piraterising <37510241+piraterising@users.noreply.github.com>
2018-07-04 23:06:54 +0100

    Added direct AJAX call

--- list_posts.js
+++ list_posts.js
@@ -6,19 +6,26 @@ $.get("article_boilerplate.html", function(raw_page)
             .replace("SOURCE", ("posts/" + i + "/thumnail.jpg"))
             .replace("ID", i)
             console.log("Before", formatted_page)
-        $.getJSON("posts/" + i + "/info.json",function(post_data){
-            post_data = post_data["POST"]
-            console.log("After", formatted_page)
-            formatted_page = formatted_page                   
-                                .replace("TITLE", post_data["TITLE"])
-                                .replace("DESCRIPTION", post_data["DESCRIPTION"])
-            $(".inner").prepend(formatted_page)
-          });
+        
+        $.ajax({
+            async: false,
+            type: 'GET',
+            url: "posts/" + i + "/info.json",
+            success: function(post_data) {
+                post_data = post_data["POST"]
+                console.log("After", formatted_page)
+                formatted_page = formatted_page                   
+                                    .replace("TITLE", post_data["TITLE"])
+                                    .replace("DESCRIPTION", post_data["DESCRIPTION"])
+                $(".inner").append(formatted_page);
+            }
+        });
+            
     }
 })
-// The loop continues whilst the loop is running, breaking the correct thumnail and ID being set. A direct AJAX call needs to be implemented with Async set to False.
 
 
 
 
 
+// loads in reverse order for some reason.
