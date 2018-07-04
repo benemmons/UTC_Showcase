439cca5d83c2b9aa43ced13c97ae51b546f4881f
piraterising <37510241+piraterising@users.noreply.github.com>
2018-07-04 23:01:48 +0100

    Added to JS, need direct AJAX rather than jquery as async problems

--- list_posts.js
+++ list_posts.js
@@ -1,15 +1,24 @@
-$.get("article_boilerplate.html", function(raw_page) {
-    for (i=0; i+=1; i<=2) { 
-            $.getJSON("posts/" + i + "/info.json", function(data){
-                post_content = data["POST"];
-                post_author = post_content["AUTHOR"]
-                post_description = post_content["DESCRIPTION"]
-                post_title = post_content["TITLE"]
-                $(".inner").prepend(raw_page
-                    .replace("SOURCE", "posts/" + i + "/thumnail.jpg")
-                    .replace("TITLE", post_title)
-                    .replace("DESCRIPTION", post_description)
-                    .replace("ID", i))
-                })   
-        }
+$.get("article_boilerplate.html", function(raw_page) 
+{
+    for (i=2; i>0; i--) 
+    { 
+        formatted_page = raw_page
+            .replace("SOURCE", ("posts/" + i + "/thumnail.jpg"))
+            .replace("ID", i)
+            console.log("Before", formatted_page)
+        $.getJSON("posts/" + i + "/info.json",function(post_data){
+            post_data = post_data["POST"]
+            console.log("After", formatted_page)
+            formatted_page = formatted_page                   
+                                .replace("TITLE", post_data["TITLE"])
+                                .replace("DESCRIPTION", post_data["DESCRIPTION"])
+            $(".inner").prepend(formatted_page)
+          });
+    }
 })
+// The loop continues whilst the loop is running, breaking the correct thumnail and ID being set. A direct AJAX call needs to be implemented with Async set to False.
+
+
+
+
+
